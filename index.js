0af005a8e50cc6c6989e9b6251bc14184aec2286
Tuyen Ha <tuyen_ha@outlook.com>
2022-06-08 18:57:27 -0600

    commit tree 9ea600aa0c1484f8b8bd38ef85b2dfb7df8606a4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution here
+function findMatching(drivers, str) {
+    const result = drivers.filter(drivers => drivers.toUpperCase() === str.toUpperCase());
+    return result;
+}
+
+function fuzzyMatch(drivers, str) {
+    const result = drivers.filter(drivers => drivers.startsWith(str));
+    return result;
+}
+
+function matchName(drivers, str) {
+    const result = drivers.filter(function(drivers) {
+        for (let driver in drivers) {
+            if (drivers.name === str || drivers.hometown === str)
+            return driver;
+        }
+    })
+    return result;
+}
+
+function matchName(drivers, str) {
+    const result = drivers.filter((drivers) => {
+        for (let driver in drivers) {
+            if (drivers.name === str || drivers.hometown === str)
+            return driver;
+        }
+    })
+    return result;
+}
